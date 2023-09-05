7704d0b1fa5b80197b0163b254f6ddfc635319f8
wangenze <15041567431@126.com>
2023-09-05 18:05:01 +0800

    :tada: 仓库初始化

---
+++ utils/res/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`你已经成功启动express服务，端口号为： ${port}`)
+})
