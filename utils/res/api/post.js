c338cd655319a8af92570193f1def3a8336a7d11
Ned <15041567431@126.com>
2023-09-23 22:47:10 +0800

    :sparkles: 支持解析excel文件

--- utils/res/api/post.js
+++ utils/res/api/post.js
@@ -1,14 +1,38 @@
-function getHeader(req){
+const xlsx = require('node-xlsx').default
+
+function getHeader(req) {
   const { header } = req.body
   return `${req.headers[header.toLowerCase()]}`
 }
 
-function getBody(req){
+function getBody(req) {
   const data = req.body
   return data
 }
 
-module.exports =  {
+function getExcelData(path) {
+  const xlsxArr = xlsx.parse(path)
+  const data = xlsxArr.map((xlsx) => {
+    return parseExcel(xlsx)
+  })
+  return data
+}
+
+function parseExcel(obj) {
+  const { data: list } = obj
+  const headers = list.shift()
+  const data = list.map((item) => {
+    const col = {}
+    for (let i = 0; i < item.length; i++) {
+      col[headers[i]] = item[i]
+    }
+    return col
+  })
+  return { headers, data }
+}
+
+module.exports = {
   getHeader,
-  getBody
+  getBody,
+  getExcelData
 }
--- utils/res/index.js
+++ utils/res/index.js
@@ -1,23 +1,36 @@
 const express = require('express')
-const { getHeader, getBody } = require('./api/post')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, './utils/res/data/store/uploads')
+  },
+  filename(req, file, cb) {
+    const suffix = file.originalname.split('.')[1]
+    cb(null, `xlsx1.${suffix}`)
+  }
+})
+
+const uploadFile = multer({ storage })
+const { getHeader, getBody, getExcelData } = require('./api/post')
 const { getParams, getTypeData, getFile } = require('./api/get')
 const app = express()
 const port = 12345
-let bodyParser = require("body-parser");
+let bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 //设置允许跨域的域名，*代表允许任意域名跨域
-app.all("*", function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*"); //允许的header类型
-  res.header("Access-Control-Allow-Headers", "*"); //跨域允许的请求方式
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*') //允许的header类型
+  res.header('Access-Control-Allow-Headers', '*') //跨域允许的请求方式
   res.header(
-    "Access-Control-Allow-Methods",
-    "PUT,POST,GET,DELETE,OPTIONS,PATCH"
-  ); //可选，用来指定本次预检请求的有效期，单位为秒。在此期间，不用发出另一条预检请求。
-  res.header("Access-Control-Max-Age", 1728000); //预请求缓存20天
-  next();
-});
+    'Access-Control-Allow-Methods',
+    'PUT,POST,GET,DELETE,OPTIONS,PATCH'
+  ) //可选，用来指定本次预检请求的有效期，单位为秒。在此期间，不用发出另一条预检请求。
+  res.header('Access-Control-Max-Age', 1728000) //预请求缓存20天
+  next()
+})
 
 const router = express.Router()
 
@@ -70,13 +83,23 @@ app.get('/api/getTypeData', (req, res) => {
 })
 
 app.get('/api/getFile', async (req, res) => {
-  const { data, contentType, file } =await getFile(req)
-  if(contentType){
-    res.setHeader('Content-Type', contentType);
-    res.setHeader('Content-Disposition', `attachment; filename=${file}`);
+  const { data, contentType, file } = await getFile(req)
+  if (contentType) {
+    res.setHeader('Content-Type', contentType)
+    res.setHeader('Content-Disposition', `attachment; filename=${file}`)
   }
   // console.log(data)
   res.send(data)
+})
+
+app.post('/uploadFile', uploadFile.single('file'), (req, res) => {
+  const data = getExcelData(req.file.path)
+  console.log(req.file)
+  res.json({
+    data,
+    status: 200,
+    message: '上传成功'
+  })
 })
 
 app.listen(port, () => {
